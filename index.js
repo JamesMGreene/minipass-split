605cf5c3dcd4112ff79bc5eb888e42f7db768a08
James M. Greene <jamesmgreene@github.com>
2020-06-04 23:20:05 -0500

    Implement first rough pass

--- index.js
+++ index.js
@@ -1,5 +1,125 @@
+// Userland modules
+const kindOf = require('kind-of')
 const Minipass = require('minipass')
 
-class SplitStream extends Minipass {}
+// Constants
+const TEXT_BUFFER = Symbol('textBuffer')
+const HANDLE_ERROR = Symbol('handleError')
+const EMITTED_ERROR = Symbol('emittedError')
+const SEPARATOR = Symbol('separator')
+const defaultSeparator = /\r?\n/
+
+class SplitStream extends Minipass {
+  constructor(options = {}) {
+    super(options)
+
+    const { separator } = options
+
+    this[TEXT_BUFFER] = ''
+    this[EMITTED_ERROR] = false
+    this[SEPARATOR] = ['string', 'regexp'].includes(kindOf(separator))
+      ? separator
+      : defaultSeparator
+  }
+
+  [HANDLE_ERROR](error, callback) {
+    this[EMITTED_ERROR] = true
+    this.emit('error', error)
+
+    if (callback) callback(error)
+
+    // Return `false` to signal backpressure
+    return false
+  }
+
+  write(chunk, encoding, callback) {
+    // Parameter shuffling
+    if (kindOf(encoding) === 'function') {
+      callback = encoding
+      encoding = null
+    }
+
+    if (kindOf(callback) !== 'function') {
+      callback = null
+    }
+
+    // If an error was already emitted, bail out immediately
+    if (this[EMITTED_ERROR]) {
+      return this[HANDLE_ERROR](new Error('Previously emitted an error'), callback)
+    }
+
+    // Convert chunk to string
+    const chunkType = kindOf(chunk)
+    let text = chunk
+    if (chunkType === 'string' && kindOf(encoding) === 'string' && encoding !== 'utf8') {
+      text = Buffer.from(chunk, encoding).toString()
+    } else if (chunkType === 'buffer') {
+      text = chunk.toString()
+    }
+
+    // If the chunk could not be converted to a string, bail out early
+    if (kindOf(text) !== 'string') {
+      return this[HANDLE_ERROR](new Error('A chunk could not be converted to a string'), callback)
+    }
+
+    // Split it up!
+    const lines = (this[TEXT_BUFFER] + text).split(this[SEPARATOR])
+
+    // Save the last partial line for the next write
+    this[TEXT_BUFFER] = lines.pop()
+
+    // Push lines onward to the next stream in the pipeline
+    let keepWriting = true
+    for (const line of lines) {
+      try {
+        keepWriting = super.write(line)
+      } catch (error) {
+        return this[HANDLE_ERROR](error, callback)
+      }
+    }
+
+    if (callback) callback()
+
+    // Return the result of the last `write` attempt to help honor backpressure
+    return keepWriting
+  }
+
+  end(chunk, encoding, callback) {
+    // Parameter shuffling
+    if (kindOf(encoding) === 'function') {
+      callback = encoding
+      encoding = null
+    } else if (kindOf(chunk) === 'function') {
+      callback = chunk
+      encoding = null
+      chunk = null
+    }
+
+    if (kindOf(callback) !== 'function') {
+      callback = null
+    }
+
+    if (chunk) {
+      this.write(chunk, encoding)
+    }
+
+    const lastLine = this[TEXT_BUFFER]
+    if (lastLine && !this[EMITTED_ERROR]) {
+      try {
+        super.write(lastLine)
+      } catch (error) {
+        this[EMITTED_ERROR] = true
+        this.emit('error', error)
+      }
+    }
+
+    return super.end(null, null, callback)
+  }
+
+  // This method will automatically be called by Minipass's `destroy` method
+  close() {
+    this.emit('close')
+  }
+}
 
 module.exports = SplitStream
--- test.js
+++ test.js
@@ -1,5 +1,225 @@
+// NOTE: These test concepts are borrowed heavily from:
+// https://github.com/mcollina/split2/blob/master/test.js
+
+// Userland modules
+const Minipass = require('minipass')
 const test = require('ava')
 
-test('foo', (t) => {
-  t.pass()
+// Local modules
+const SplitStream = require('.')
+
+test('reads and writes Buffers by default', async (t) => {
+  const input = new Minipass()
+  const transform = new SplitStream()
+
+  input.pipe(transform)
+  input.end(Buffer.from('hello\nworld'))
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.deepEqual(lines[0], Buffer.from('hello'))
+  t.deepEqual(lines[1], Buffer.from('world'))
+})
+
+test('reads strings if encoding is provided to preceding input stream', async (t) => {
+  const input = new Minipass({ encoding: 'utf8' })
+  const transform = new SplitStream()
+
+  input.pipe(transform)
+  input.end('hello\nworld')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.deepEqual(lines[0], Buffer.from('hello'))
+  t.deepEqual(lines[1], Buffer.from('world'))
+})
+
+test('writes strings if encoding is provided', async (t) => {
+  const input = new Minipass()
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  input.pipe(transform)
+  input.end(Buffer.from('hello\nworld'))
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('reads and writes strings if encoding is provided to surrounding input and output streams', async (t) => {
+  const input = new Minipass({ encoding: 'utf8' })
+  const transform = new SplitStream()
+  const output = new Minipass({ encoding: 'utf8' })
+
+  input.pipe(transform).pipe(output)
+  input.end('hello\nworld')
+
+  const lines = await output.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('splits two lines on end', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.end('hello\nworld')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('splits two lines on two writes', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.write('hello')
+  transform.write('\nworld')
+  transform.end()
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('splits four lines on three writes', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.write('hello\nwor')
+  transform.write('ld\nbye\nwo')
+  transform.write('rld')
+  transform.end()
+
+  const lines = await transform.collect()
+  t.is(lines.length, 4)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+  t.is(lines[2], 'bye')
+  t.is(lines[3], 'world')
+})
+
+test('splits four lines on three writes and end', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.write('hello\nwor')
+  transform.write('ld\nbye\nwo')
+  transform.write('rl')
+  transform.end('d')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 4)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+  t.is(lines[2], 'bye')
+  t.is(lines[3], 'world')
+})
+
+test('split lines when the "\\n" comes at the end of a chunk', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.write('hello\n')
+  transform.end('world')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('accumulates multiple writes', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.write('hello')
+  transform.write('world')
+  transform.end()
+
+  const lines = await transform.collect()
+  t.is(lines.length, 1)
+  t.is(lines[0], 'helloworld')
+})
+
+test('splits using a custom string separator', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8', separator: '~' })
+
+  transform.end('hello~world')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('splits using a custom RegExp separator', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8', separator: /~/ })
+
+  transform.end('hello~world')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('splits lines for Windows-style separators', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.end('hello\r\nworld')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 2)
+  t.is(lines[0], 'hello')
+  t.is(lines[1], 'world')
+})
+
+test('does not split two lines at "\\r" only on end', async (t) => {
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  transform.end('hello\rworld')
+
+  const lines = await transform.collect()
+  t.is(lines.length, 1)
+  t.is(lines[0], 'hello\rworld')
+})
+
+test('has a destroy method that signals closure', async (t) => {
+  t.plan(1)
+
+  const transform = new SplitStream({ encoding: 'utf8' })
+
+  await new Promise((resolve, reject) => {
+    function onClose() {
+      t.pass('close emitted')
+
+      // Remove the other listener for cleanliness
+      transform.removeListener('error', onError)
+
+      resolve()
+    }
+
+    function onError(error) {
+      // Remove the other listener for cleanliness
+      transform.removeListener('close', onClose)
+
+      reject(error)
+    }
+
+    transform.once('close', onClose)
+    transform.once('error', onError)
+
+    transform.destroy()
+  })
+})
+
+test('does not modify the options object', async (t) => {
+  const options = { encoding: 'utf8' }
+  const transform = new SplitStream(options)
+
+  transform.end()
+
+  const lines = await transform.collect()
+  t.is(lines.length, 0)
+  t.deepEqual(options, { encoding: 'utf8' })
 })
